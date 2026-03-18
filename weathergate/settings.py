44249d749c080ee08835d6ee90a6963d4efd5f47
Hamilton <hamiltonpcreis.eng@gmail.com>
2026-03-18 15:48:45 +0000

    feat: configure PostgreSQL database and run initial migrations

--- weathergate/settings.py
+++ weathergate/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'weathergate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weathergate_db',
+        'USER': 'weathergate_user',
+        'PASSWORD': 'weathergate2026',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
