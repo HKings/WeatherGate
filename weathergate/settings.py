61fae78cdae55e7a7cc362052a847dd116c2d680
Hamilton <hamiltonpcreis.eng@gmail.com>
2026-05-25 13:54:21 +0100

    fix: add railway.app to ALLOWED_HOSTS

--- weathergate/settings.py
+++ weathergate/settings.py
@@ -12,7 +12,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Security
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', '.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
